4fb75d0a0274d73b828f9034169fe8a6161ab569
Matias <lit.io30303@gmail.com>
2025-05-16 14:22:32 -0400

    pagina principal add widget

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,4 +4,22 @@
 <div class="row">
     <h1>Panel Principal</h1>
 </div>
+
+<hr>
+<div class="row">
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>123</h3>
+
+                <p>Usuarios</p>
+            </div>
+            <div class="icon">
+                <i class="ion fas bi bi-file-person"></i>
+            </div>
+            <a href="#" class="small-box-footer">Mas información <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+</div>
 @endsection
